5c1544657cc187790b53bd0a4c5ab8bf79e85eec
Jonathan Thamrun <Jonathan.Thamrun@goevo.com>
2022-11-14 16:38:46 -0800

    Update to Header.tsx in app dir

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <header className='p-5 bg-blue-500'>
-        <p className='text-white font-bold'>I am a Header</p>
+    <header className="flex p-5 bg-base-green space-x-10">
+      <h1 className="font-quicksand font-bold text-2xl">spill.</h1>
+      <div className="flex space-x-10">
+        <h1>Create Expense</h1>
+        <h1>Past Expenses</h1>
+        <h1>Friends</h1>
+        <h1>Search People</h1>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      "base-green": "#7FD6AA",
+    },
+    extend: {
+      fontFamily: {
+        quicksand: ["quicksand", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
