e6247f6acfe29eaa1fbfe5759a429c03554a549c
Christopher Bryan <chrisbryann@Christophers-MacBook-Pro.local>
2023-07-31 10:55:32 -0700

    replaced usePathname() with useParams(), solved querying path parameters

--- app/expense/edit/[editId]/page.tsx
+++ app/expense/edit/[editId]/page.tsx
@@ -1,15 +1,15 @@
 "use client"
-import { usePathname } from "next/navigation"
+import { useParams } from "next/navigation"
 import { useEffect, useState } from "react";
 import EditExpenseComponent from "../../../../components/Expense/EditExpenseComponent";
 
 const editExpensePage = () => {
-    const pathname = usePathname()!;
+    const params = useParams()!;
   const [editId, setEditId] = useState("");
 
   useEffect(() => {
-     setEditId(pathname.split("/").pop() as string)
-  }, [pathname])
+     setEditId(params.editId as string)
+  }, [params])
 
   return <EditExpenseComponent />
 }
--- app/search/friends/[uid]/page.tsx
+++ app/search/friends/[uid]/page.tsx
@@ -1,15 +1,15 @@
 "use client";
-import { usePathname } from "next/navigation";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import FriendProfileComponent from "../../../../components/SearchFriends/FriendProfileComponent";
 
 const FriendProfilePage = () => {
-  const pathname = usePathname()!;
+  const params = useParams()!;
   const [id, setID] = useState("");
 
   useEffect(() => {
-     setID(pathname.split("/").pop() as string)
-  }, [pathname])
+     setID(params.uid as string)
+  }, [params])
 
   return (
     <div className="h-[72vh] px-10 py-5 grid overflow-auto md:max-w-2xl xl:max-w-3xl m-auto">
