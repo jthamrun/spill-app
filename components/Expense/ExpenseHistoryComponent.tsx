0de027fbb46a6cbc899615d4f9f12e2bba18c12f
Jonathan Thamrun <jthm@jonathans-mini.lan>
2023-07-31 23:59:52 -0700

    Expense History Mobile UI Update

--- components/Expense/ExpenseHistoryComponent.tsx
+++ components/Expense/ExpenseHistoryComponent.tsx
@@ -4,85 +4,100 @@ import { useEffect, useState } from "react";
 import { db } from "../../firebase.config";
 import { AnySessionProps, Expense, ExpenseItem } from "../store/types";
 import ExpenseCard from "./ExpenseCard";
+import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 
 const ExpenseHistoryComponent = ({ session }: AnySessionProps) => {
-  const [expenses, setExpenses] = useState<Expense[]>();
-  useEffect(() => {
-    const getExpenses = async () => {
-      // get past expenses of the current user (ongoing, finished, cancelled(?) )
-      const expenseList: Expense[] = [];
-      await getDocs(
-        query(
-          collection(db, "expenses"),
-          or(
-            where("creator_id", "==", session.user.id),
-            where("users", "array-contains", session.user.id)
-          )
-        )
-      ).then((snapshot) => {
-        snapshot.forEach((doc) => {
-          const data = doc.data();
-          const exp: Expense = {
-            expense_id: doc.id,
-            creator_id: data.creator_id,
-            users: data.users,
-            subtotal_amount: data.subtotal_amount,
-            total_amount: data.total_amount,
-            tax_amount: data.tax_amount,
-            tip_amount: data.tip_amount,
-            status: data.status,
-            items: [],
-          };
+    const [expenses, setExpenses] = useState<Expense[]>();
+    const [search, setSearch] = useState("");
 
-          // find each of the expense items in database
-          data.items.forEach(async (item: string) => {
+    useEffect(() => {
+        const getExpenses = async () => {
+            // get past expenses of the current user (ongoing, finished, cancelled(?) )
+            const expenseList: Expense[] = [];
             await getDocs(
-              query(
-                collection(db, "expense-items"),
-                where("__name__", "==", item)
-              )
-            ).then((item_snapshot) => {
-              item_snapshot.forEach((item_doc) => {
-                const item_data = item_doc.data();
-                exp.items?.push({
-                  item_id: item,
-                  name: item_data.name,
-                  amount: item_data.amount,
-                  ordered_by: item_data.ordered_by, // should we only collect list of user_id or the users object list?
+                query(
+                    collection(db, "expenses"),
+                    or(
+                        where("creator_id", "==", session.user.id),
+                        where("users", "array-contains", session.user.id)
+                    )
+                )
+            ).then((snapshot) => {
+                snapshot.forEach((doc) => {
+                    const data = doc.data();
+                    const exp: Expense = {
+                        expense_id: doc.id,
+                        creator_id: data.creator_id,
+                        users: data.users,
+                        subtotal_amount: data.subtotal_amount,
+                        total_amount: data.total_amount,
+                        tax_amount: data.tax_amount,
+                        tip_amount: data.tip_amount,
+                        status: data.status,
+                        items: [],
+                    };
+
+                    // find each of the expense items in database
+                    data.items.forEach(async (item: string) => {
+                        await getDocs(
+                            query(
+                                collection(db, "expense-items"),
+                                where("__name__", "==", item)
+                            )
+                        ).then((item_snapshot) => {
+                            item_snapshot.forEach((item_doc) => {
+                                const item_data = item_doc.data();
+                                exp.items?.push({
+                                    item_id: item,
+                                    name: item_data.name,
+                                    amount: item_data.amount,
+                                    ordered_by: item_data.ordered_by, // should we only collect list of user_id or the users object list?
+                                });
+                            });
+                        });
+                    });
+
+                    expenseList.push(exp);
                 });
-              });
             });
-          });
 
-          expenseList.push(exp);
-        });
-      });
+            setExpenses(expenseList);
+        };
 
-      setExpenses(expenseList);
-    };
+        getExpenses();
+    }, []);
 
-    getExpenses();
-  }, []);
-
-  return (
-    <div className="flex flex-col items-center pt-8 space-y-6">
-      <div className="flex flex-col border border-black rounded-md w-[70vw] items-center">
-        <h1 className="py-4 font-quicksand font-bold">Past Expenses</h1>
-        <div className="h-[1px] bg-black w-full"></div>
-        <h3 className="py-6 font-quicksand font-semibold">
-          <span className="font-bold">3</span> Expenses
-        </h3>
-      </div>
-      <div className="flex flex-col border border-black rounded-md w-[90vw] items-center">
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 w-full py-4 px-6">
-          <ExpenseCard />
-          <ExpenseCard />
-          <ExpenseCard />
-          <ExpenseCard />
+    return (
+        <div className="flex flex-col items-center pt-5 space-y-5">
+            <div className="border border-black rounded-md w-[70vw] text-center">
+                <h1 className="py-4 font-quicksand font-bold">Past Expenses</h1>
+                <div className="h-[1px] bg-black w-full"></div>
+                <h3 className="py-6 font-quicksand font-medium">
+                    <span className="font-bold">3</span> Expenses
+                </h3>
+            </div>
+            <div className="h-[72vh] overflow-auto border border-black rounded-md w-[90vw] space-y-4 p-4">
+                <div className="flex items-center p-2 h-10 w-full rounded-md pace-x-2 border border-black focus-within:ring-1 focus-within:ring-black">
+                    <input
+                        className="font-quicksand text-search-bar-gray focus:outline-none w-full"
+                        type="text"
+                        placeholder="Search Expense"
+                        value={search}
+                        onChange={(e) => {
+                            setSearch(e.target.value);
+                        }}
+                    />
+                    <MagnifyingGlassIcon className="h-5" />
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-4 w-full">
+                    <ExpenseCard />
+                    <ExpenseCard />
+                    <ExpenseCard />
+                    <ExpenseCard />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ExpenseHistoryComponent;
